4f64da4e064e27f98f6bf4a4a3148e8eff62ac2d
Temo <temoog@gmail.com>
2010-10-14 00:11:18 +0900

    serversman check tool

---
+++ monitServersman.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import sys, urllib, urllib2, cookielib, commands, datetime
+
+#### 設定開始 #########################################
+
+# ServersMan管理ツール
+smAdminParam = {
+    'passwd'   : '管理ツールのパスワード',
+    'usr_name' : 'admin',
+    'q'        : 'auth',
+}
+
+# ServersManのIDとパスワード
+smAccParam = {
+    'sm-account'  : 'ServersMan用メールアドレス',
+    'sm-password' : 'ServersManのパスワード',
+    'q'           : 4,
+    'setSMState'  : 0,
+    'smSwitch'    : 1,
+    'B1'          : '設定'
+}
+
+#### 設定終了 #########################################
+
+# ServersManのURL
+smUrl = 'http://localhost/serversman'
+
+# プロセス監視するコマンド
+chkstr = 'ps -ef | grep "/opt/serversman/bin/serversman" | grep -v "grep"'
+
+
+result = commands.getoutput(chkstr)
+if result:
+    sys.exit();
+
+
+cookie = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+
+# トップページでログイン
+try:
+    result = opener.open(
+        smUrl,
+        urllib.urlencode(smAdminParam)
+    )
+except:
+    print sys.exc_info()
+    sys.exit()
+
+# 設定・変更ページからServersManを「有効」にする
+try:
+    result = opener.open(
+        smUrl,
+        urllib.urlencode(smAccParam)
+    )
+except:
+    print sys.exc_info()
+    sys.exit()
+
+# リスタートした日時を出力
+now = datetime.datetime.now()
+print now.strftime("%Y-%m-%d %H:%M:%S")
+
